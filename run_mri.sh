ad5a95e5d3fad932c83636b5d3102ab2ee55c343
cederican <cedimac00@icloud.com>
2023-04-18 18:45:07 +0200

    Batchfile adjustments

--- run_mri.sh
+++ run_mri.sh
@@ -3,14 +3,10 @@
 #SBATCH --output=output_diffae.log      # Ausgabedatei
 #SBATCH --error=errors_diffae.log       # Fehlerdatei
 #SBATCH --gres=gpu:2
-#SBATCH --cpus-per-gpu=4 
-#SBATCH --mem-per-gpu=32GB
-#SBATCH --nodes=2
-#SBATCH --ntasks=4
 #SBATCH --time=1:00:00
 
 export NCCL_DEBUG=INFO
 export PYTHONFAULTHANDLER=1
 
 
-srun --gres=gpu:volta:2 python run_mri.py
+srun --time=1:00:00 --gres=gpu:volta:2 python run_mri.py
